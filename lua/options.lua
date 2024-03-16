19af9f8afb111d2e79f2ae9a68f1ba4d3b797382
JoeJoeC <yoseph@pop-os.gateway>
2024-03-17 01:29:22 +0800

    Added Lazy Git

--- lua/options.lua
+++ lua/options.lua
@@ -1,10 +1,12 @@
 --[[Neovim Options]]--
 vim.g.mapleader = " " -- space is assigned as mapleader
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true -- set to true for nerd fonts
 
 local opt = vim.opt
 
 opt.termguicolors = true -- enable 24 bits color in terminal
-opt.showmode = true -- shows the mode
+opt.showmode = false -- shows the mode
 opt.tabstop = 4 -- a tab is worth 4 spaces
 opt.shiftwidth = 4 -- indents are 4 spaces
 opt.softtabstop = 4 -- a tab is 4 spaces in insert mode
@@ -19,3 +21,8 @@ opt.fillchars = "eob: " -- Changes the line '~' into spaces
 opt.autochdir = true -- change directory to the file in current window
 opt.clipboard ="unnamedplus" -- enables clipboard from registry
 opt.title = true -- use the title of the current file in the buffer
+opt.undofile = true -- save undofiles
+opt.ignorecase = true -- case-insensitive searching unless \C or capital in the search
+opt.smartcase = true
+opt.splitright = true -- new splits can split right
+opt.splitbelow = true -- new splits can split below
---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,15 @@
+return {
+    "kdheepak/lazygit.nvim",
+    name = "Lazygit",
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilerCurrentFile",
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+}
+
