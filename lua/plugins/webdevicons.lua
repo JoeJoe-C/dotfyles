968eba92078fdf52f10cc8cd8ec42c9641c7f4e8
JoeJoeC <yoseph@pop-os.gateway>
2024-03-17 01:37:21 +0800

    Added webdevicons!

---
+++ lua/plugins/webdevicons.lua
@@ -0,0 +1,7 @@
+return{
+    "nvim-tree/nvim-web-devicons",
+    name = "WebDev Icons",
+    url = "https://github.com/nvim-tree/nvim-web-devicons.git",
+    enabled = vim.g.have_nerd_font, -- Ensures that this is enabled!
+}
+
