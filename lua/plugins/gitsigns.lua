1ab951c6880307804d6ffe7706acda38711d3962
JoeJoeC <yoseph@pop-os.gateway>
2024-03-17 01:26:36 +0800

    Updated Gitsigns signs

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,5 +2,14 @@ return {
     "lewis6991/gitsigns.nvim",
     name="Git Signs",
     url="https://github.com/lewis6991/gitsigns.nvim",
+    opts= {
+        signs = {
+            add = { text = "+" },
+            change = { text = "~" },
+            delete = { text = "_" },
+            topdelete = { text = "‾" },
+            changedelete = { text = "~" },
+        },
+    },
 }
 
